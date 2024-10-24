475ed4d82a19f00aea11ee49f768aae69961634a
zhouzhou725 <ucfnsz5@ucl.ac.uk>
2024-10-24 16:33:07 +0100

    third commit

--- week4.R
+++ week4.R
@@ -2,8 +2,8 @@ library(sf)
 library(dplyr)
 library(readr)
 
-inequality_data <- read_csv("C:/Users/zhouzhou/Desktop/GIS/wk4/homework/hdr-data.csv")
-world_geojson <- st_read("C:/Users/zhouzhou/Desktop/GIS/wk4/homework/World_Countries_(Generalized)_9029012925078512962.geojson")
+inequality_data <- read_csv("C:/Users/zhouzhou/Desktop/GIS/wk4/homework/week4_homework/gii.csv")
+world_geojson <- st_read("C:/Users/zhouzhou/Desktop/GIS/wk4/homework/week4_homework/World_Countries_.geojson")
 
 head(inequality_data)
 head(world_geojson)
@@ -13,10 +13,15 @@ head(world_geojson)
 print(names(world_geojson))
 print(inequality_data)
 
+inequality_data <- inequality_data %>% rename(COUNTRY = country)
+colnames(world_geojson)
+colnames(inequality_data)
+
 
 
 inequality_data <- inequality_data %>%
-  mutate(Inequality_Difference = 2019 - 2010)
+  mutate(Inequality_Difference = gii_2010 - gii_2019)
 merged_data <- world_geojson %>%
   left_join(inequality_data, by = "COUNTRY")
+
 head(merged_data)
